00f502f3326c2564937e90c9fda209f9f82d0c11
Eshaan Rana <97769730+seetheworlda350@users.noreply.github.com>
2024-10-02 15:03:47 -0700

    10/2/24
    Completed some work for lesson 2 on 10/2/2024

--- Unit 2/lesson2/Main.java
+++ Unit 2/lesson2/Main.java
@@ -16,12 +16,13 @@ public class Main
         // make a custom cup of coffee
         Coffee myCup = new Coffee(5,false,"no");
         System.out.println(myCup);
+        
+        // make a default cup of coffee
         Coffee myDefaultCup2 = new Coffee();
         System.out.println("Default "+myDefaultCup2);
-        // make a default cup of coffee
-        
         // make a default pinata
-        
+        Pinata myPinata = new Pinata();
+        System.out.println(myPinata);
         // make more pinatas to test all of the constructors you made
         
         // test each of the website constructors
